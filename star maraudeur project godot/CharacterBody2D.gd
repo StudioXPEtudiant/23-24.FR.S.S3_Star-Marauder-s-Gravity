8e5fbbdab146bf8b19b91c78331a63ec5a583773
Lycialkr <lililakhdari3@gmail.com>
2024-05-25 13:05:05 +0200

    mise a jour

---
+++ star maraudeur project godot/CharacterBody2D.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var MAXSpeed = 300
+@export var ACCELERATION = 1500
+@export var FRICTION = 1200
+
+var axis = Vector2.ZERO
+
+func _physics_process(delta):
+	move(delta)
+
+func get_input_axis():
+	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	return axis.normalized()
+
+func move(delta):
+	axis = get_input_axis()
+	
+	if axis == Vector2.ZERO:
+		apply_friction(FRICTION * delta)
+	else:
+		apply_movement(axis * ACCELERATION * delta)
+	
+	move_and_slide()
+
+func apply_friction(amount):
+	var friction_direction = -velocity.normalized()
+	var friction_force = friction_direction * min(amount, velocity.length())
+	velocity += friction_force
+
+func apply_movement(force):
+	velocity += force
+	velocity = velocity.normalized() * MAXSpeed
+
+
+
+
+
